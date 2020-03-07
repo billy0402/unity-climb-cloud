5af59ce218020e1ae5569e865c943eb341b14354
Billy Huang <10646003@ntub.edu.tw>
2020-03-07 21:25:08 +0800

    6-8 - 讓攝影機配合遊戲角色移動

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    private GameObject _player;
+
+    // Start is called before the first frame update
+    void Start() {
+        this._player = GameObject.Find("cat");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // 確認遊戲角色的 Y 座標
+        Vector3 playerPos = this._player.transform.position;
+        // 調整攝影機的 Y 座標
+        transform.position = new Vector3(transform.position.x, playerPos.y, transform.position.z);
+    }
+}
