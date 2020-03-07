a7c649eefd36f0c55710f63b6af94b0d95f06777
Billy Huang <10646003@ntub.edu.tw>
2020-03-07 19:27:51 +0800

    6-5 - 讓遊戲角色依照指示動作

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    private Rigidbody2D _rigid2D;
+    private float _jumpForce = 680.0f;
+    private float _walkForce = 30.0f;
+    private float _maxWalkSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        this._rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // 跳躍
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            this._rigid2D.AddForce(transform.up * this._jumpForce);
+        }
+
+        // 左右移動
+        int key = 0;
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        // 遊戲角色的速度
+        float speedX = Mathf.Abs(this._rigid2D.velocity.x);
+
+        // 速度限制
+        if (speedX < this._maxWalkSpeed) {
+            this._rigid2D.AddForce(transform.right * this._walkForce * key);
+        }
+
+        // 依照行進方向翻轉
+        if (key != 0) {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+    }
+}
